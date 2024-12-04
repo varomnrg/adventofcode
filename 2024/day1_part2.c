d5a7b00761cb8dd2515b3a73715558d706397871
varomnrg <varomnrg@gmail.com>
2024-12-04 18:07:35 +0700

    add day 1 part 2

--- 2024/day1_part2.c
+++ 2024/day1_part2.c
@@ -1,14 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sort(int arr[], int n);
+// Structure for dictionary
+typedef struct
+{
+    int key;
+    int value;
+} Dict;
+
+// Function for initializing the dictionary
+Dict* initDict(int *capacity){
+    *capacity = 10; 
+    Dict *dict = (Dict *)malloc((*capacity) * sizeof(Dict));
+    if (dict == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+
+    return dict;
+}
+
+// Function for resizing the dictionary 
+Dict* resizeDict(Dict *dict, int *capacity){
+    *capacity *= 2;
+    dict = (Dict *)realloc(dict, (*capacity) * sizeof(Dict));
+    if (dict == NULL) {
+        printf("Memory reallocation failed!\n");
+        exit(1);
+    }
+    return dict;
+}
+
+// Function for getting the value of the dictionary with the key
+int getVal(Dict *dict, int size, int key){
+    for (int i = 0; i < size; i++){
+        if(dict[i].key == key){
+            return dict[i].value;
+        }
+    }
+
+    return 0;
+}
+
+
+// Function for incrementing the value of the dictionary
+/*
+    Function for incrementing the value of the dictionary
+    - Using double pointer to modify the dict for resizing purposes
+    - 
+*/
+void increment(Dict **dict, int *size, int *capacity, int key){
+    for (int i = 0; i < *size; i++){
+        if((*dict)[i].key == key){
+            (*dict)[i].value++;
+            return;
+        }
+    }
+
+    // Resize the dicct if capacity is not enough
+    if (*size >= *capacity) {
+        *dict = resizeDict(*dict, capacity);
+    }
+    
+    // Add the new key to the dict
+    (*dict)[*size].key = key;
+    (*dict)[*size].value = 1;  
+    (*size)++;
+}
+    
 
 int main(){
+    // Start capacity for the array
     int capacity = 10;
-    int *list1 = (int *)malloc(capacity * sizeof(int));
-    int *list2 = (int *)malloc(capacity * sizeof(int));
+
+    // Created 2 dynamic array
+    int *arr1 = (int *)malloc(capacity * sizeof(int));
+    int *arr2 = (int *)malloc(capacity * sizeof(int));
     
-    if (list1 == NULL || list2 == NULL){
+    if (arr1 == NULL || arr2 == NULL){
         printf("Memory allocation failed\n");
         return 1;
     }
@@ -22,49 +91,45 @@ int main(){
             break;
         }
 
+        // grow the memory by * 2 if the total row count > initial capacity
         if (row_count >= capacity) {
-            capacity *= 1.5;
-            list1 = (int *)realloc(list1, capacity * sizeof(int));
-            list2 = (int *)realloc(list2, capacity * sizeof(int));
+            capacity *= 2;
+            arr1 = (int *)realloc(arr1, capacity * sizeof(int));
+            arr2 = (int *)realloc(arr2, capacity * sizeof(int));
 
-            if (list1 == NULL || list2 == NULL) {
+            if (arr1 == NULL || arr2 == NULL) {
                 fprintf(stderr, "Memory reallocation failed\n");
                 return 1;
             }
         }
 
-        list1[row_count] = a;
-        list2[row_count] = b;
+        arr1[row_count] = a;
+        arr2[row_count] = b;
         row_count++;
     }
 
-    int total = 0;
-    sort(list1, row_count);
-    sort(list2, row_count);
+    int capacityDict = 10;
+    int sizeDict = 0;
 
+    Dict *dict = initDict(&capacityDict);
+
+    for (int i = 0; i < row_count; i++) {
+        increment(&dict, &sizeDict, &capacityDict, arr2[i]);
+    }
+    
+    int total = 0;
 
     for(int i = 0; i < row_count ; i++){
-        int distance = list2[i] - list1[i];
-        total += abs(distance);
+        int value = arr1[i];
+        total += value * getVal(dict, sizeDict, arr1[i]);
     }
 
     printf("Total Distance : ");
     printf("%i\n", total);
 
-    free(list1);
-    free(list2);
+    free(arr1);
+    free(arr2);
+    free(dict);
 
     return 0;
-}
-
-void sort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
 }
